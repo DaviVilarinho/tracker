94c4198c93666859e7b0eb310f6a0c185c4597d5
Davi Vilarinho <davi.vilarinho@prax.net.br>
2024-04-15 11:39:21 -0300

    editing view corrected

--- src/views/EditProjetoView.vue
+++ src/views/EditProjetoView.vue
@@ -8,7 +8,9 @@ import ProjectForm from '@/components/ProjectForm.vue';
 import { key } from '@/store';
 import { useRouter } from 'vue-router';
 import { useStore } from 'vuex';
-import { onMounted, ref, defineProps } from 'vue';
+import { ref, defineProps, onBeforeMount } from 'vue';
+import useNotificar from '@/hooks/notificar';
+import { AppNotificationType } from '@/interfaces/INotification';
 import type { Project } from '@/interfaces/IProject.vue';
 
 const router = useRouter();
@@ -17,14 +19,13 @@ const props = defineProps({ id: String });
 
 const project = ref<Project | undefined>();
 
-onMounted(() => {
-  try {
-    if (store.state.projects.get(props.id as string)?.id !== props.id) {
-      throw new Error('Invalid param');
-    }
-    project.value = store.state.projects.get(props.id as string);
-  } catch (e) {
-    router.push({ name: 'projects' });
+try {
+  if (store.state.projects.get(props.id as string)?.id !== props.id) {
+    throw new Error('Invalid param');
   }
-});
+  project.value = store.state.projects.get(props.id as string);
+} catch (e) {
+  useNotificar('Não foi possível editar', 'Esta página não existe', AppNotificationType.DANGER);
+  router.push({ name: 'TableView' });
+}
 </script>
