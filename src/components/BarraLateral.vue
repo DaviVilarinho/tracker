32b7a50e7b28f378fc552fe388b6313b0140e5ef
Davi Vilarinho <davi.vilarinho@prax.net.br>
2024-04-16 09:59:52 -0300

    barra lateral changes

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -1,29 +1,27 @@
 <template>
-  <section class="section columns">
-    <div class="container column is-three-fifths text-center" style="height: auto !important; white-space: unset !important; width: 100%">
-      <h1 class="title">
-        Tracker
-      </h1>
-      <hr>
-      <p class="subtitle">
-        Seu organizador de tarefas.
-      </p>
-      <nav class="panel mt-5 p-4 navpad">
-        <ul>
-          <router-link to="/" class="link">
-            <i class="fa fa-tasks"></i>
-            Tarefas
-          </router-link>
-        </ul>
-        <ul>
-          <router-link to="/projetos" class="link">
-            <i class="fa fa-briefcase"></i>
-            Projetos
-          </router-link>
-        </ul>
-      </nav>
-      <button class="button" @click.prevent="emit('toggleTheme')">Modo {{ isDarkMode ? 'Claro' : 'Escuro'  }}</button>
-    </div>
+  <section class="text-center section is-flex is-justify-content-space-evenly is-flex-wrap-nowrap is-align-content-space-around is-flex-direction-column" style="width: 100%; height: 100%;">
+        <h1 class="title">
+          Tracker
+        </h1>
+        <hr>
+        <p class="subtitle">
+          Seu organizador de tarefas.
+        </p>
+        <nav class="navpad">
+          <ul>
+            <router-link to="/" class="link">
+              <i class="fa fa-tasks"></i>
+              Tarefas
+            </router-link>
+          </ul>
+          <ul>
+            <router-link to="/projetos" class="link">
+              <i class="fa fa-briefcase"></i>
+              Projetos
+            </router-link>
+          </ul>
+        </nav>
+        <button class="button" @click.prevent="emit('toggleTheme')">Modo {{ isDarkMode ? 'Claro' : 'Escuro' }}</button>
   </section>
 </template>
 
@@ -43,8 +41,9 @@ const isDarkMode = computed(() => store.state.isDarkMode);
   text-align: center;
 }
 
-.navpad > ul {
+.navpad>ul {
   padding-top: .5rem;
   padding-bottom: .5rem;
+  text-align: left;
 }
 </style>
